b71b2e85f8e516d2ee9d1d96b1586efe461ea9a7
Jose Nunes <josenunes34@gmail.com>
2019-07-28 21:31:39 +0100

    feat: testing cz cli
    Lorem Ipsum Dolor
    
    closes #1

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 var uniqueRandomArray = require('unique-random-array');
 var starWarsNames = require('./starwars-names.json');
 
+// Test
 module.exports = {
   all: starWarsNames,
   random: uniqueRandomArray(starWarsNames),
