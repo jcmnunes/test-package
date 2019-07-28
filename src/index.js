2793cab2d4a06876473a969d755542d8ae174d53
Jose Nunes <josenunes34@gmail.com>
2019-07-28 21:35:22 +0100

    feat: lorem ipsum
    Lorem ipsum dolor sit amet

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 var uniqueRandomArray = require('unique-random-array');
 var starWarsNames = require('./starwars-names.json');
 
-// Test
+// Test 2
 module.exports = {
   all: starWarsNames,
   random: uniqueRandomArray(starWarsNames),
